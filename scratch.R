208fb6560f2e879e4f9ed686d81feb6ad46e01f8
Ed Yu <eddytheflow@gmail.com>
2023-06-11 12:35:20 -0400

    updated exports

--- scratch.R
+++ scratch.R
@@ -1,17 +1,23 @@
 # IMPORT ----
 # import with COVID information (to filter), and inpatient/outpatient/ER ONLY
 
-## TX_2021_01.csv - COUNTY (county population and ALL deaths)
+## TX_2021_01.csv - COUNTY (County population and ALL deaths)
 ## TX_2021_02.csv - COUNTY (All Deaths EXCEPT at Decedents home / Other)
+
 ## TX_2021_03.csv - COUNTY > Month (All Deaths EXCEPT at Decedents home / Other)
 ## TX_2021_04.csv - COUNTY > Month (COVID Deaths EXCEPT at Decedents home / Other)
+
+## TX_2021_05.csv - COUNTY > Month > Weekday (All Deaths EXCEPT at Decedents home / Other)
+## TX_2021_06.csv - COUNTY > Month > Weekday (COVID Deaths EXCEPT at Decedents home / Other)
 
 library(tidyverse)
 library(janitor)
 library(magrittr)
 library(readr)
 
-## raw code to make data ----
+## Raw Code ----
+
+### Annual and Monthly ----
 
 state="TX"
 year="2021"
@@ -20,6 +26,8 @@ path1=paste0("data/", state, "_", year, "_01.csv")
 path2=paste0("data/", state, "_", year, "_02.csv")
 path3=paste0("data/", state, "_", year, "_03.csv")
 path4=paste0("data/", state, "_", year, "_04.csv")
+path5=paste0("data/", state, "_", year, "_05.csv")
+path6=paste0("data/", state, "_", year, "_06.csv")
 
 d1 <- read_csv(path1) %>% 
   clean_names() %>% 
@@ -68,9 +76,35 @@ d4 <- read_csv(path4) %>%
 export_filename = paste0(state, "_", year, "_all")
 write_csv(d4, paste0("export/", export_filename, ".csv"))
 
-## Function to do it ----
+### Weekday ----
 
-make_dataset <- function(state, year, export){
+d5 <- read_csv(path5) %>% 
+  clean_names() %>% 
+  mutate(
+    month = month(as.numeric(gsub(month_sub, "", month_code)), label=TRUE)
+  ) %>% 
+  select(county_code, month, weekday, deaths) %>% 
+  set_colnames(c("county_code", "month", "weekday", "wkday_hospital_deaths")) %>% 
+  drop_na()
+
+d6 <- read_csv(path6) %>% 
+  clean_names() %>% 
+  mutate(
+    month = month(as.numeric(gsub(month_sub, "", month_code)), label=TRUE)
+  ) %>% 
+  select(county_code, month, weekday, deaths) %>% 
+  set_colnames(c("county_code", "month", "weekday", "covid_wkday_hospital_deaths")) %>% 
+  drop_na()
+
+d6 <- d5 %>% left_join(d6)
+
+d6 %>% 
+  arrange(-as.numeric(covid_wkday_hospital_deaths))
+
+
+## Functions ----
+
+make_monthly_dataset <- function(state, year, export){
   
   path1=paste0("data/", state, "_", year, "_01.csv")
   path2=paste0("data/", state, "_", year, "_02.csv")
@@ -122,11 +156,55 @@ make_dataset <- function(state, year, export){
     select(year, state, county, county_code, annual_county_population, annual_all_deaths, annual_hospital_deaths, month, monthly_hospital_deaths, monthly_covid_hospital_deaths)
   
   if(export){
-    export_filename = paste0(state, "_", year, "_all")
+    export_filename = paste0(state, "_", year, "_monthly")
     write_csv(d4, paste0("export/", export_filename, ".csv"))
   }
   
   return(d4)
 }
 
-make_dataset(state = "TX", year = "2021", export = FALSE)
+make_monthly_dataset(state = "TX", year = "2021", export = TRUE)
+
+make_wkday_dataset <- function(state, year, export){
+  
+  path5=paste0("data/", state, "_", year, "_05.csv")
+  path6=paste0("data/", state, "_", year, "_06.csv")
+  
+  month_sub = paste0(year, "/")
+  
+  d5 <- read_csv(path5) %>% 
+    clean_names() %>% 
+    mutate(
+      month = month(as.numeric(gsub(month_sub, "", month_code)), label=TRUE)
+    ) %>% 
+    select(county_code, month, weekday, deaths) %>% 
+    set_colnames(c("county_code", "month", "weekday", "wkday_hospital_deaths")) %>% 
+    drop_na()
+  
+  d6 <- read_csv(path6) %>% 
+    clean_names() %>% 
+    mutate(
+      month = month(as.numeric(gsub(month_sub, "", month_code)), label=TRUE)
+    ) %>% 
+    select(county_code, month, weekday, deaths) %>% 
+    set_colnames(c("county_code", "month", "weekday", "covid_wkday_hospital_deaths")) %>% 
+    drop_na()
+  
+  d6 <- d5 %>% left_join(d6)
+  
+  if(export){
+    export_filename = paste0(state, "_", year, "_wkday")
+    write_csv(d6, paste0("export/", export_filename, ".csv"))
+  }
+ 
+  return(d6)
+   
+}
+
+make_wkday_dataset(state = "TX", year = "2021", export = TRUE)
+
+# EDA ----
+
+## Suppressed? ----
+
+TX_2021_monthly
