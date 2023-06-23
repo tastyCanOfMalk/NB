c4a35a57542dbcc0d377fac634fd32e8d96d371b
Ed Yu <eddytheflow@gmail.com>
2023-06-23 11:47:10 -0400

    modify month format, export modified data to /export2

--- data_prep.R
+++ data_prep.R
@@ -1,3 +1,5 @@
+rm(list = ls())
+
 # IMPORT ----
 # import with COVID information (to filter), and inpatient/outpatient/ER ONLY
 
@@ -29,82 +31,81 @@ library(readr)
 # path5=paste0("data/", state, "_", year, "_05.csv")
 # path6=paste0("data/", state, "_", year, "_06.csv")
 # 
-# d1 <- read_csv(path1) %>% 
-#   clean_names() %>% 
-#   select(county, county_code, population, deaths) %>% 
-#   set_colnames(c("county", "county_code", "annual_county_population", "annual_all_deaths")) %>% 
+# d1 <- read_csv(path1) %>%
+#   clean_names() %>%
+#   select(county, county_code, population, deaths) %>%
+#   set_colnames(c("county", "county_code", "annual_county_population", "annual_all_deaths")) %>%
 #   drop_na()
 # 
-# d2 <- read_csv(path2) %>% 
+# d2 <- read_csv(path2) %>%
 #   clean_names() %>%
-#   select(county_code, deaths) %>% 
-#   set_colnames(c("county_code", "annual_hospital_deaths")) %>% 
-#   drop_na() %>% 
-#   left_join(d1) %>% 
+#   select(county_code, deaths) %>%
+#   set_colnames(c("county_code", "annual_hospital_deaths")) %>%
+#   drop_na() %>%
+#   left_join(d1) %>%
 #   select(county, county_code, annual_county_population, annual_all_deaths, annual_hospital_deaths)
 # 
 # month_sub = paste0(year, "/")
 # 
-# d3 <- read_csv(path3) %>% 
-#   clean_names() %>% 
-#   select(county_code, month_code, deaths) %>% 
+# d3 <- read_csv(path3) %>%
+#   clean_names() %>%
+#   select(county_code, month_code, deaths) %>%
 #   mutate(
 #     month = month(as.numeric(gsub(month_sub, "", month_code)), label=TRUE)
-#   ) %>% 
-#   select(county_code, month, deaths) %>% 
-#   set_colnames(c("county_code", "month", "monthly_hospital_deaths")) %>% 
-#   drop_na() %>% 
-#   right_join(d2) %>% 
+#   ) %>%
+#   select(county_code, month, deaths) %>%
+#   set_colnames(c("county_code", "month", "monthly_hospital_deaths")) %>%
+#   drop_na() %>%
+#   right_join(d2) %>%
 #   select(county, county_code, annual_county_population, annual_all_deaths, annual_hospital_deaths, month, monthly_hospital_deaths)
 # 
-# d4 <- read_csv(path4) %>% 
-#   clean_names() %>% 
-#   select(county_code, month_code, deaths) %>% 
+# d4 <- read_csv(path4) %>%
+#   clean_names() %>%
+#   select(county_code, month_code, deaths) %>%
 #   mutate(
 #     month = month(as.numeric(gsub(month_sub, "", month_code)), label=TRUE)
-#   ) %>% 
-#   select(county_code, month, deaths) %>% 
-#   set_colnames(c("county_code", "month", "covid_monthly_hospital_deaths")) %>% 
-#   drop_na() %>% 
-#   right_join(d3) %>% 
+#   ) %>%
+#   select(county_code, month, deaths) %>%
+#   set_colnames(c("county_code", "month", "covid_monthly_hospital_deaths")) %>%
+#   drop_na() %>%
+#   right_join(d3) %>%
 #   mutate(year = as.numeric(year),
 #          state = as.factor(state),
 #          county = as.factor(gsub(" County, TX", "", county))
-#   ) %>% 
+#   ) %>%
 #   select(year, state, county, county_code, annual_county_population, annual_all_deaths, annual_hospital_deaths, month, monthly_hospital_deaths, covid_monthly_hospital_deaths)
 # 
 # export_filename = paste0(state, "_", year, "_all")
 # write_csv(d4, paste0("export/", export_filename, ".csv"))
-
-### Weekday ----
-
-# d5 <- read_csv(path5) %>% 
-#   clean_names() %>% 
+# 
+# ### Weekday ----
+# 
+# d5 <- read_csv(path5) %>%
+#   clean_names() %>%
 #   mutate(
+#     year = year,
+#     state = state,
 #     month = month(as.numeric(gsub(month_sub, "", month_code)), label=TRUE)
-#   ) %>% 
-#   select(county_code, month, weekday, deaths) %>% 
-#   set_colnames(c("county_code", "month", "weekday", "wkday_hospital_deaths")) %>% 
+#   ) %>%
+#   select(state, year, county_code, month, weekday, deaths) %>%
+#   set_colnames(c("county_code", "month", "weekday", "wkday_hospital_deaths")) %>%
 #   drop_na()
 # 
-# d6 <- read_csv(path6) %>% 
-#   clean_names() %>% 
+# d6 <- read_csv(path6) %>%
+#   clean_names() %>%
 #   mutate(
 #     month = month(as.numeric(gsub(month_sub, "", month_code)), label=TRUE)
-#   ) %>% 
-#   select(county_code, month, weekday, deaths) %>% 
-#   set_colnames(c("county_code", "month", "weekday", "covid_wkday_hospital_deaths")) %>% 
+#   ) %>%
+#   select(county_code, month, weekday, deaths) %>%
+#   set_colnames(c("county_code", "month", "weekday", "covid_wkday_hospital_deaths")) %>%
 #   drop_na()
 # 
 # d6 <- d5 %>% left_join(d6)
 # 
-# d6 %>% 
+# d6 %>%
 #   arrange(-as.numeric(covid_wkday_hospital_deaths))
 
-
-
-
-## Using Functions ----
+## Functions ----
 
 make_monthly_dataset <- function(state, year, export){
   
@@ -125,6 +126,10 @@ make_monthly_dataset <- function(state, year, export){
     set_colnames(c("county_code", "annual_hospital_deaths")) %>% 
     drop_na() %>% 
     left_join(d1) %>% 
+    mutate(
+      annual_all_deaths = as.character(annual_all_deaths),
+      annual_hospital_deaths = as.character(annual_hospital_deaths)
+      ) %>% 
     select(county, county_code, annual_county_population, annual_all_deaths, annual_hospital_deaths)
   
   month_sub = paste0(year, "/")
@@ -133,7 +138,7 @@ make_monthly_dataset <- function(state, year, export){
     clean_names() %>% 
     select(county_code, month_code, deaths) %>% 
     mutate(
-      month = month(as.numeric(gsub(month_sub, "", month_code)), label=TRUE)
+      month = month(as.numeric(gsub(month_sub, "", month_code)), label=FALSE)
     ) %>% 
     select(county_code, month, deaths) %>% 
     set_colnames(c("county_code", "month", "monthly_hospital_deaths")) %>% 
@@ -147,7 +152,7 @@ make_monthly_dataset <- function(state, year, export){
     clean_names() %>% 
     select(county_code, month_code, deaths) %>% 
     mutate(
-      month = month(as.numeric(gsub(month_sub, "", month_code)), label=TRUE)
+      month = month(as.numeric(gsub(month_sub, "", month_code)), label=FALSE)
     ) %>% 
     select(county_code, month, deaths) %>% 
     set_colnames(c("county_code", "month", "monthly_covid_hospital_deaths")) %>% 
@@ -174,20 +179,24 @@ make_wkday_dataset <- function(state, year, export){
   path6=paste0("data/", state, "_", year, "_06.csv")
   
   month_sub = paste0(year, "/")
+  county_sub = paste0(" County, ", state)
   
   d5 <- read_csv(path5) %>% 
     clean_names() %>% 
     mutate(
-      month = month(as.numeric(gsub(month_sub, "", month_code)), label=TRUE)
+      year = year,
+      state = state,
+      county = as.factor(gsub(county_sub, "", county)),
+      month = month(as.numeric(gsub(month_sub, "", month_code)), label=FALSE)
     ) %>% 
-    select(county_code, month, weekday, deaths) %>% 
-    set_colnames(c("county_code", "month", "weekday", "wkday_hospital_deaths")) %>% 
+    select(year, state, county, county_code, month, weekday, deaths) %>% 
+    set_colnames(c("year", "state", "county", "county_code", "month", "weekday", "wkday_hospital_deaths")) %>% 
     drop_na()
   
   d6 <- read_csv(path6) %>% 
     clean_names() %>% 
     mutate(
-      month = month(as.numeric(gsub(month_sub, "", month_code)), label=TRUE)
+      month = month(as.numeric(gsub(month_sub, "", month_code)), label=FALSE)
     ) %>% 
     select(county_code, month, weekday, deaths) %>% 
     set_colnames(c("county_code", "month", "weekday", "covid_wkday_hospital_deaths")) %>% 
@@ -204,7 +213,6 @@ make_wkday_dataset <- function(state, year, export){
   }
  
   return(d6)
-   
 }
 
 to_remove = list()
@@ -215,7 +223,7 @@ load_datasets <- function(state, year, export = FALSE){
   assign(name_monthly, make_monthly_dataset(state = state, year = year, export = export), envir = parent.frame())
 
   name_wkday = paste0(state, "_", year, "_wkday")
-  assign(name_wkday, make_monthly_dataset(state = state, year = year, export = export), envir = parent.frame())
+  assign(name_wkday, make_wkday_dataset(state = state, year = year, export = export), envir = parent.frame())
 
   to_remove <<- append(to_remove, name_monthly)  
   to_remove <<- append(to_remove, name_wkday)  
@@ -226,35 +234,56 @@ export_datasets <- function(state, year, export = TRUE){
   make_wkday_dataset(state = state, year = year, export = export)
 }
 
-export_datasets(state = "TX", year = "2021")
-export_datasets(state = "TX", year = "2020")
-export_datasets(state = "TX", year = "2019")
-export_datasets(state = "TX", year = "2018")
+run_all_exports <- function(){
+  export_datasets(state = "TX", year = "2021")
+  export_datasets(state = "TX", year = "2020")
+  export_datasets(state = "TX", year = "2019")
+  export_datasets(state = "TX", year = "2018")
+  
+  export_datasets(state = "OK", year = "2021")
+  export_datasets(state = "OK", year = "2020")
+  export_datasets(state = "OK", year = "2019")
+  export_datasets(state = "OK", year = "2018")
+}
+  
+## Manual loading ----
+
 load_datasets  (state = "TX", year = "2021")
 load_datasets  (state = "TX", year = "2020")
 load_datasets  (state = "TX", year = "2019")
 load_datasets  (state = "TX", year = "2018")
 
-export_datasets(state = "OK", year = "2021")
-export_datasets(state = "OK", year = "2020")
-export_datasets(state = "OK", year = "2019")
-export_datasets(state = "OK", year = "2018")
 load_datasets  (state = "OK", year = "2021")
 load_datasets  (state = "OK", year = "2020")
 load_datasets  (state = "OK", year = "2019")
 load_datasets  (state = "OK", year = "2018")
 
-OK_all <- bind_rows(list(OK_2018_monthly,
-                         OK_2019_monthly,
-                         OK_2020_monthly,
-                         OK_2021_monthly))
-TX_all <- bind_rows(list(TX_2018_monthly,
-                         TX_2019_monthly,
-                         TX_2020_monthly,
-                         TX_2021_monthly))
+OK_all_monthly <- bind_rows(list(OK_2018_monthly,
+                                 OK_2019_monthly,
+                                 OK_2020_monthly,
+                                 OK_2021_monthly))
+TX_all_monthly <- bind_rows(list(TX_2018_monthly,
+                                 TX_2019_monthly,
+                                 TX_2020_monthly,
+                                 TX_2021_monthly))
 
-write_csv(OK_all, paste0("export/OK_all.csv"))
-write_csv(TX_all, paste0("export/TX_all.csv"))
+OK_all_wkday <- bind_rows(list(OK_2018_wkday,
+                               OK_2019_wkday,
+                               OK_2020_wkday,
+                               OK_2021_wkday))
+TX_all_wkday <- bind_rows(list(TX_2018_wkday,
+                               TX_2019_wkday,
+                               TX_2020_wkday,
+                               TX_2021_wkday))
+
+export_all <- function(){
+  write_csv(OK_all_monthly, paste0("export/OK_all_monthly.csv"))
+  write_csv(TX_all_monthly, paste0("export/TX_all_monthly.csv"))
+  write_csv(OK_all_wkday,   paste0("export/OK_all_wkday.csv"))
+  write_csv(TX_all_wkday,   paste0("export/TX_all_wkday.csv"))
+}
+
+# export_all()
 
 remove(list = to_remove %>% unlist())
 remove(to_remove)
---
+++ data_prep_pt2.R
@@ -0,0 +1,72 @@
+# Continuation of data cleaning and formatting
+# Substitute `Suppressed` with numeric value 5
+# Keep only relevant death data (in-hospital AND non-COVID deaths)
+# Break up datasets into annual/monthly/wkday datasets so they can be joined as needed
+
+# Run initial data prep
+source("data_prep.R")
+
+# Join states ----
+df_monthly <- bind_rows(OK_all_monthly, 
+                        TX_all_monthly)
+
+df_wkday <- bind_rows(OK_all_wkday, 
+                      TX_all_wkday)
+
+# Replace suppressed ----
+df_monthly[df_monthly == "Suppressed"] <- "5"  
+df_wkday[df_wkday == "Suppressed"] <- "5"  
+
+# Create annual  DF ----
+df_annual <- df_monthly %>% 
+  left_join(
+    df_monthly %>% 
+      group_by(year, state, county, county_code) %>% 
+      summarise(annual_covid_hospital_deaths = sum(as.numeric(monthly_covid_hospital_deaths)))
+  ) %>% 
+  mutate(
+    county_code = as.factor(county_code),
+    annual_adj_deaths = as.numeric(annual_hospital_deaths) - as.numeric(annual_covid_hospital_deaths),
+    annual_crude_rate_10k = annual_adj_deaths/annual_county_population*10000
+    ) %>% 
+  select(year, state, county, county_code, annual_county_population, annual_adj_deaths, annual_crude_rate_10k) %>% 
+  set_colnames(c("year", "state", "county", "county_code", "county_population", "anunal_adj_deaths", "annual_crude_rate_10k")) %>%
+  unique()
+
+# Create monthly DF ----
+df_monthly <- df_monthly %>% 
+  mutate(
+    date = ymd(paste0(year, "-", month, "-01")),
+    monthly_adj_deaths = as.numeric(monthly_hospital_deaths) - as.numeric(monthly_covid_hospital_deaths),
+    monthly_crude_rate_10k = monthly_adj_deaths/annual_county_population*10000
+  ) %>% 
+  select(year, month, date, state, county, county_code, monthly_adj_deaths, monthly_crude_rate_10k)
+
+# Create wkday DF ----
+county_pop <- df_annual %>% select(year, state, county_code, county_population)
+
+df_wkday <- df_wkday %>% 
+  mutate(
+    year = as.numeric(year),
+    state = as.factor(state),
+    county_code = as.factor(county_code)
+  ) %>% 
+  left_join(county_pop) %>% 
+  mutate(
+    wkday = as.factor(weekday),
+    wkday_adj_deaths = as.numeric(wkday_hospital_deaths) - as.numeric(covid_wkday_hospital_deaths),
+    wkday_crude_rate_10k = wkday_adj_deaths/county_population*10000
+  ) %>% 
+  select(year, month, wkday, state, county, county_code, county_population, wkday_adj_deaths, wkday_crude_rate_10k)
+
+# Export ----
+
+export_dfs <- function(){
+  write_csv(df_wkday,   paste0("export2/df_wkday.csv"))
+  write_csv(df_monthly, paste0("export2/df_monthly.csv"))
+  write_csv(df_annual,  paste0("export2/df_annual.csv"))
+}
+
+# export_dfs()
+
+
